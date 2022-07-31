531c1af612d125363f066bfeaeac50c70fc48382
Jose Carballo <jacarballorojas@teco.com.ar>
2022-07-31 13:07:20 -0300

    agregado metodos de autenticacion y funciones para el seteo del token

---
+++ src/helpers/index.ts
@@ -0,0 +1,7 @@
+
+export const addTokenCredential = (token:any) => localStorage.setItem("accessToken", token);
+
+export const getTokenCredencial = () => localStorage.getItem('accessToken');
+
+export const logoutTokenCredential = () => localStorage.removeItem("accessToken");
+
