f6d4af7683f14d6e9b56468832bb98186f84c602
Liam Marin <liam.marin03926@protonmail.com>
2022-08-01 06:10:51 -0300

    simplificar componentes AdminNotFound

--- src/routes/admin/NotFound.tsx
+++ src/routes/admin/NotFound.tsx
@@ -1,25 +1,25 @@
-import Box from "@mui/material/Box";
+import { styled } from "@mui/material";
 import Link from "@mui/material/Link";
 import Typography from "@mui/material/Typography";
 import { Link as RouterLink } from "react-router-dom";
 
+const Container = styled("div")({
+  minHeight: "100%",
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
+});
+
 export default function AdminNotFound() {
   return (
-    <Box
-      sx={{
-        minHeight: "100%",
-        display: "flex",
-        alignItems: "center",
-        justifyContent: "center",
-      }}
-    >
-      <Box>
+    <Container>
+      <div>
         <Typography variant="h1">404</Typography>
         <Typography gutterBottom>Página no encontrada.</Typography>
         <Link component={RouterLink} to="/admin">
           Volver al Dashboard
         </Link>
-      </Box>
-    </Box>
+      </div>
+    </Container>
   );
 }
